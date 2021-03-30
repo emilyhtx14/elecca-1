b044fc3d8b5e393d3a5b6070dfa40cc3c3a0784b
Emily Huang <71594652+emilyhtx14@users.noreply.github.com>
2021-03-30 11:00:22 -0500

    final ver

---
+++ matmul/constants.h
@@ -0,0 +1,21 @@
+/**
+    Header file for constants.
+
+    @file constants.h
+    @author Emily Huang
+    @email emh13@rice.edu
+    @date 03/30/2021
+*/
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+//matrix dimensions
+
+#define A_ROWS      4
+#define A_COLS      3
+
+#define B_ROWS      A_COLS
+#define B_COLS      4
+
+#endif // CONSTANTS_H
